11bcf2144c8ae569727c99aaaf98afb028bef79d
douglas <douglas@10.146.195.172>
2026-04-14 13:46:56 -0300

    fix: Comentário de linhas

--- src/main/java/com/mycompany/av3_controle_estoque_algoritmos/Main.java
+++ src/main/java/com/mycompany/av3_controle_estoque_algoritmos/Main.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.av3_controle_estoque_algoritmos;
 
-/**
- *
- * @author douglas
- */
 public class Main {
 
     public static void main(String[] args) {
